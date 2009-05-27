720e13328200f9af9734241dc22eff32c77b5836
chengyf <chengyf@e2d178541d40466.hs.handsome.com.cn>
2009-05-27 13:16:14 +0800

    dd

---
+++ TestGit/src/org/goodev/test/GitTest.java
@@ -0,0 +1,13 @@
+package org.goodev.test;
+
+public class GitTest {
+
+	/**
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubd
+		System.out.println("Git Test...");
+	}
+
+}
